43ea919865197dfe5ccd3a47978550f7bfe3ccf7
DESKTOP-JQD9108\User <denistrigub66@gmail.com>
2022-02-16 20:42:30 +0200

    for () created

--- Project30/Source.cpp
+++ Project30/Source.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 int main()
 {
+	srand(time(0));
+	const int size = 10;
+	int arr[size];
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % -20 - 20;
+		cout << arr[i] << endl;
+	}
 	return 0;
 }

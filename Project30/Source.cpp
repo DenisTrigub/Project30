66a34e52c7ed112689c273055c1df84888d3e593
DESKTOP-JQD9108\User <denistrigub66@gmail.com>
2022-02-16 19:50:17 +0200

    Добавьте файлы проекта.

---
+++ Project30/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	cout << "#------------Menu-------------#" << endl;
+	cout << "#      |---->1-Ex1<----|      #" << endl;
+	cout << "#      |---->2-Ex2<----|      #" << endl;
+	cout << "#------------3-Ex3------------#" << endl;
+	int J = 4;
+	cin >> J;
+	switch (J)
+	{
+	case 1:
+	{
+		cout << "\a";
+	}break;
+	case 2:
+	{
+
+	}break;
+	}
+}
